fe4ac7efe73d8128a76676ca3035fdaf8864fdc4
FairKill07 <nasatov.oleg03@gmail.com>
2023-03-28 13:52:34 +0300

    frut_icon

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const sigiInBtn = document.querySelector('#signin_btn');
+const sigiUnBtn = document.querySelector("#signup_btn");
+
+
+const form = document.querySelector(".form-box");
+const body = document.body;
+
+
+sigiUnBtn.addEventListener('click', () => {
+	form.classList.add('active');
+	body.classList.add('active');
+});
+
+sigiInBtn.addEventListener('click', () => {
+	form.classList.remove('active');
+	body.classList.remove('active');
+})
+
+// $(".phone").mask("+38(999)999-9999");
